a6f4ece9f0224c61503acfcf793dcc4cf9b7e6bd
Jorge Valdez <georch@octahedroid.com>
2020-06-02 12:23:57 -0500

    Update Storybook configuration

---
+++ .storybook/preview.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { action } from "@storybook/addon-actions"
+import { addDecorator, addParameters } from "@storybook/react"
+import { ThemeProvider } from "theme-ui"
+import { DocsPage, DocsContainer } from "@storybook/addon-docs/blocks"
+import theme from "../src/theme"
+
+// Gatsby's Link overrides:
+// Gatsby defines a global called ___loader to prevent its method calls from creating console errors you override it here
+global.___loader = {
+  enqueue: () => {},
+  hovering: () => {},
+}
+// Gatsby internal mocking to prevent unnecessary errors in storybook testing environment
+global.__PATH_PREFIX__ = ""
+// This is to utilized to override the window.___navigate method Gatsby defines and uses to report what path a Link would be taking us to if it wasn't inside a storybook
+window.___navigate = (pathname) => {
+  action("NavigateTo:")(pathname)
+}
+
+addDecorator((Story) => (
+  <ThemeProvider theme={theme}>
+    <Story />
+  </ThemeProvider>
+))
+
+addParameters({
+  docs: {
+    container: DocsContainer,
+    page: DocsPage,
+  },
+})
