959e10fae6b9a34186ad2775fa95a6bb7cf2ed90
luisduenas <luis@octahedroid.com>
2020-06-03 09:08:05 -0700

    feat: add eslint & husky

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      pragma: "React",
+      version: "detect",
+    },
+  },
+  extends: ["airbnb", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  rules: {
+    "react/jsx-filename-extension": [
+      1,
+      {
+        extensions: [".js", ".jsx"],
+      },
+    ],
+    "import/no-cycle": 0,
+    "no-underscore-dangle": 0,
+    "jsx-a11y/anchor-is-valid": 0,
+  },
+}
