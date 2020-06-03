ff2f1a0eb0b7ce9fe21de2ad956655cf77222b09
Jorge Valdez <georch@octahedroid.com>
2020-06-03 09:57:28 -0500

    Create Slice component

---
+++ src/components/Slice/index.js
@@ -0,0 +1,26 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { Box } from "theme-ui"
+
+const Slice = ({ children, ...props }) => {
+  return (
+    <Box
+      as="section"
+      __css={{
+        width: "100vw",
+        position: "relative",
+        px: ["cPadding", null, "hPadding"],
+        py: "vSpace",
+      }}
+      {...props}
+    >
+      {children}
+    </Box>
+  )
+}
+
+Slice.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Slice
