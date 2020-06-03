43b8436e92403976d4bdf9bbb8a135c23a9dfc9a
Jorge Valdez <georch@octahedroid.com>
2020-06-03 09:57:51 -0500

    Create Desc component

---
+++ src/components/Desc/index.js
@@ -0,0 +1,32 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { Box } from "theme-ui"
+
+const Desc = ({ children, ...props }) => {
+  return (
+    <Box
+      __css={{
+        mx: [null, null, 'auto'],
+        mt: ["vMargin", null, "vMargin2x"],
+        maxWidth: '42rem',
+        fontSize: 'lg',
+        fontWeight: 'lean',
+        'p': {
+          lineHeight: 'desc',
+        },
+        'p:last-of-type': {
+          mb: '0',
+        },
+      }}
+      {...props}
+    >
+      {children}
+    </Box>
+  )
+}
+
+Desc.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Desc
