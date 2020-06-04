ae436577f51ebb5b764181f197f8bedcc560b10b
Joseph Ramos <jardix22@gmail.com>
2020-06-03 23:59:01 -0500

    Refactor call to action component.

--- src/slices/CallToAction/index.js
+++ src/slices/CallToAction/index.js
@@ -1,25 +1,53 @@
 import React from 'react'
 import { array, func, shape } from 'prop-types'
-
 import { RichText } from 'prismic-reactjs'
-import { Button, Wrap } from '../../components'
+import { Box } from 'theme-ui'
+
+import { Button, Wrap, Desc, Slice } from '../../components'
 
 const CallToAction = ({
   slice: { primary },
 
   linkResolver, // linkResolver is pased by SliceZone
 }) => {
+  const styles = {
+    header: {
+      mb: [null, 'hPadding'],
+    },
+    icon: {
+      width: 'full',
+      maxWidth: '120px',
+      mb: 'cMargin',
+      mx: 'auto',
+
+      img: {
+        width: '100%',
+      },
+    },
+  }
+
   return (
-    <Wrap>
-      <RichText render={primary.title} />
-      {primary.paragraph && <RichText render={primary.paragraph} />}
-      <Button
-        variant="primary"
-        link={primary.button_link}
-        label={primary.button_label}
-        resolver={linkResolver}
-      />
-    </Wrap>
+    <Slice sx={{ textAlign: 'center' }}>
+      <Wrap>
+        <Box as="header" sx={styles.header}>
+          {primary.icon_image && (
+            <Box __css={styles.icon}>
+              <img src={primary.icon_image.url} alt={primary.icon_image.alt} />
+            </Box>
+          )}
+          <RichText render={primary.title} />
+        </Box>
+        <Desc>
+          {primary.paragraph && <RichText render={primary.paragraph} />}
+        </Desc>
+        <Button
+          variant="primary"
+          link={primary.button_link}
+          label={primary.button_label}
+          resolver={linkResolver}
+        />
+      </Wrap>
+    </Slice>
   )
 }
 
