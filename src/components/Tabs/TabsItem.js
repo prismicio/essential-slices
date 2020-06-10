89e15e9576501ed2a0601525ca4d9dbf29b8fc83
luisduenas <luis@octahedroid.com>
2020-06-10 09:32:13 -0700

    feat: add tabs component

---
+++ src/components/Tabs/TabsItem.js
@@ -0,0 +1,51 @@
+import React, { cloneElement } from 'react'
+import PropTypes from 'prop-types'
+import { Box } from 'theme-ui'
+
+import { getBaseTabsItemStyles } from './styles'
+
+const TabsItem = ({ active, disabled, label, variant, onActivate }) => {
+  const baseTabsItemStyles = getBaseTabsItemStyles(disabled)
+
+  const tabsItemStyles = {
+    ...baseTabsItemStyles,
+  }
+
+  const onClickHandler = () => {
+    if (onActivate) onActivate()
+  }
+
+  const defaultProps = !disabled ? { onClick: onClickHandler } : {}
+  const className = active ? 'active' : undefined
+
+  return (
+    <Box
+      variant={`${variant}.item`}
+      __themeKey="tabs"
+      __css={tabsItemStyles}
+      className={className}
+      {...defaultProps}
+    >
+      {cloneElement(label, {
+        className,
+      })}
+    </Box>
+  )
+}
+
+TabsItem.defaultProps = {
+  active: false,
+  disabled: false,
+  variant: 'default',
+  onActivate: undefined,
+}
+
+TabsItem.propTypes = {
+  active: PropTypes.bool,
+  disabled: PropTypes.bool,
+  variant: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  onActivate: PropTypes.func,
+}
+
+export default TabsItem
